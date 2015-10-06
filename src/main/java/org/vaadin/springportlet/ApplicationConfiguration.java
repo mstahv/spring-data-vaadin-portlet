a4c2121336c555c3b9030b869eef29fe5b99431c
Matti Tahvonen <matti@vaadin.com>
2015-10-06 18:51:58 +0300

    Using Spring Boot configured DataSource

--- src/main/java/org/vaadin/springportlet/ApplicationConfiguration.java
+++ src/main/java/org/vaadin/springportlet/ApplicationConfiguration.java
@@ -1,20 +1,30 @@
 package org.vaadin.springportlet;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import com.vaadin.spring.VaadinConfiguration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @Import(VaadinConfiguration.class)
 @ComponentScan("org.vaadin.springportlet")
-public class ApplicationConfiguration {
-	
-    @Configuration
-    @PropertySource("classpath:application.properties")
-    static class ApplicationProperties {}
-    
+@EnableJpaRepositories
+public class ApplicationConfiguration extends SpringBootServletInitializer {
+
+	@Configuration
+	@PropertySource("classpath:application.properties")
+	static class ApplicationProperties {
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApplicationConfiguration.class);
+	}
+
 }
